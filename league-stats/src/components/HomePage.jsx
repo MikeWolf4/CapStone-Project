408636740d7a3a51645726df3ad39ca3a4ea7f6c
Michael Wolford <wolfordmichael04@gmail.com>
2020-06-26 13:38:10 -0700

    Added Standings

--- league-stats/src/components/HomePage.jsx
+++ league-stats/src/components/HomePage.jsx
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import Select from "react-select";
 import Logo from "../assets/header/logo.svg";
 import { Link } from "react-router-dom";
+import Cloud9Logo from "../assets/teams/Cloud9logo.png";
+import HundredThieves from "../assets/teams/100_Thieveslogo.png";
+import Counter_Logic from "../assets/teams/Counter_Logic_Gaminglogo.png";
+import Dignitas from "../assets/teams/Dignitaslogo.png";
+import Eg from "../assets/teams/Eglogo.png";
+import FlyQuest from "../assets/teams/FlyQuestlogo.png";
+import Golden_Guardians from "../assets/teams/Golden_Guardianslogo.png";
+import Immortals from "../assets/teams/Immortalslogo.png";
+import Team_Liquid from "../assets/teams/Team_Liquidlogo.png";
+import Team_SoloMid from "../assets/teams/Team_SoloMidlogo.png";
 
 const options = [
   { value: "NA1", label: "NA" },
@@ -73,6 +83,51 @@ class HomePage extends Component {
 
           <button className="homepage__search-button">search</button>
         </form>
+        <div className="event__container">
+          <div>
+            <h2>LCS 2020 Summer Standings</h2>
+            <p>
+              1: <img src={Cloud9Logo} />
+              ⁠Cloud9 4 - 0
+            </p>
+            <p>
+              2: <img src={Counter_Logic} />
+              ⁠Counter Logic Gaming 3 - 1
+            </p>
+            <p>
+              2: <img src={Eg} />
+              ⁠⁠Evil Geniuses 3 - 1
+            </p>
+            <p>
+              2: <img src={FlyQuest} />
+              ⁠⁠FlyQuest 3 - 1
+            </p>
+            <p>
+              2: <img src={Team_Liquid} />
+              ⁠⁠Team Liquid 3 - 1
+            </p>
+            <p>
+              6: <img src={Team_SoloMid} />
+              ⁠⁠Team SoloMid 2 - 2
+            </p>
+            <p>
+              7: <img src={HundredThieves} />
+              ⁠⁠100 Thieves 1 - 3
+            </p>
+            <p>
+              7: <img src={Golden_Guardians} />
+              ⁠⁠Golden Guardians 1 - 3
+            </p>
+            <p>
+              9: <img src={Dignitas} />
+              ⁠⁠Dignitas 0 - 4
+            </p>
+            <p>
+              9: <img src={Immortals} />
+              Immortals 0 - 4
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
